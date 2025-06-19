a41d2ab8c74405e39ee88b5d921c338a14e9256d
Marinelic <75620592+Marinelic@users.noreply.github.com>
2025-06-19 22:25:15 +0200

    Adding new sections

--- app/root.tsx
+++ app/root.tsx
@@ -9,6 +9,7 @@ import {
 
 import type { Route } from "./+types/root";
 import "./app.css";
+import Navbar from "./components/navbar";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -41,9 +42,25 @@ export function Layout({ children }: { children: React.ReactNode }) {
   );
 }
 
+
+
+
+
+
 export default function App() {
-  return <Outlet />;
-}
+  return (
+    <div>
+      <Navbar />
+      <Outlet />
+    </div>
+  );
+};
+
+
+
+
+
+
 
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
   let message = "Oops!";
