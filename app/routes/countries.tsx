523ca6b6aa8205b2858eeb5331f92b57f93e48c9
Marinelic <75620592+Marinelic@users.noreply.github.com>
2025-07-02 19:11:54 +0200

    Adding Countries

--- app/routes/countries.tsx
+++ app/routes/countries.tsx
@@ -1,3 +1,39 @@
-export default function Countries () {
-    return <div>Countries Page</div>
+import type { Route } from "./+types/countries";
+import React from "react";
+import { Link } from "react-router";
+
+
+export async function clientLoader () {
+    const res = await fetch("https://restcountries.com/v3.1/all?fields=name,flags,capital,region,population");
+    const data = await res.json();
+    return data;
+}
+
+
+export default function Countries ({ loaderData }: Route.ComponentProps) {
+
+    console.log(loaderData);
+    return (
+        <div>
+
+            <h2>Countries</h2>
+
+            <div>
+                <input type="text" placeholder="Search by names..." />
+            </div>
+
+            <ul>
+                {loaderData.map((country: any, key: number) => (
+                    <li 
+                        key={key}>
+                        <Link to={`/countries/${country.name.common}`}> {country.name.common} </Link>
+
+                        <div>
+                            Region: {country.region} | Population: {country.population}
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
 }
